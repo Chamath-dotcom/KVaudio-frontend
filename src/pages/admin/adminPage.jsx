15c003fe87cc976e49d2cb6bb52720944f896fb6
Chamath Kalhara <ckalhara360@gmail.com>
2025-02-12 16:57:44 +0530

    add some tooltips for adminPage✅

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -1,40 +1,45 @@
-import { MdDashboard } from "react-icons/md";
-import { FaSwatchbook } from "react-icons/fa";
-import { BiSolidSpeaker } from "react-icons/bi";
-import { FaUsers } from "react-icons/fa";
+import { Tooltip } from "react-tooltip";
 import { Link, Route, Routes } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+import { FaSwatchbook, FaUsers } from "react-icons/fa";
+import { BiSolidSpeaker } from "react-icons/bi";
 
+export default function AdminPage() {
+  return (
+    <div className="w-full h-screen flex">
+      <div className="w-[4vw] h-full border flex flex-col justify-start gap-6 items-center text-[20px]">
+        <Link to="/admin/dashboard" className="mt-5 " data-tooltip-id="dashboard-tooltip">
+          <MdDashboard />
+        </Link>
+        <Tooltip id="dashboard-tooltip" place="right" content="Go to Dashboard" />
 
+        <Link to="/admin/bookings"  data-tooltip-id="bookings-tooltip">
+          <FaSwatchbook />
+        </Link>
+        <Tooltip id="bookings-tooltip" place="right" content="View Bookings" />
 
-export default function AdminPage(){
-    return(
-        <div className="w-full h-screen flex">
-            <div className="w-[10vw] h-full border-2 flex flex-col">
-                <Link className="">
-                    <MdDashboard/>
-                    Dashboard
-                </Link>
-                <Link to="/admin/bookings" className="">
-                    <FaSwatchbook/>
-                    Bookings
-                </Link>
-                <Link to="/admin/items" className="">
-                    <BiSolidSpeaker/>
-                    Items
-                </Link>
-                <Link className="">
-                    <FaUsers/>
-                    Users
-                </Link>
-            </div>
-        <div className="w-[calc(100vw-200px)] ">
-            <Routes path="/*">
-            <Route path="/bookings" element={<h1>Booking</h1>}/>
-            <Route path="/items" element={<h1>item</h1>}/> 
-            <Route path="/items/add" element={<h1>add item</h1>}/>
-            <Route path="/items/edit" element={<h1>Booking</h1>}/>
-            </Routes>
-        </div>
+        <Link to="/admin/items"  data-tooltip-id="items-tooltip">
+          <BiSolidSpeaker />
+        </Link>
+        <Tooltip id="items-tooltip" place="right" content="Manage Items" />
+
+        <Link to="/admin/users"  data-tooltip-id="users-tooltip">
+          <FaUsers />
+        </Link>
+        <Tooltip id="users-tooltip" place="right" content="Manage Users" />
+      </div>
+
+      {/* Main Content */}
+      <div className="w-[calc(100vw-10vw)] ">
+        <Routes>
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/bookings" element={<h1>Bookings</h1>} />
+          <Route path="/items" element={<h1>Items</h1>} />
+          <Route path="/items/add" element={<h1>Add Item</h1>} />
+          <Route path="/items/edit" element={<h1>Edit Item</h1>} />
+          <Route path="/users" element={<h1>Users</h1>} />
+        </Routes>
+      </div>
     </div>
-    )
+  );
 }
