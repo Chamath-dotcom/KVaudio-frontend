31b014af3ce857020d12e2d270538ee5d2c96770
Chamath Kalhara <ckalhara360@gmail.com>
2025-02-12 12:24:01 +0530

    start create adminPage✅

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -1,7 +1,40 @@
+import { MdDashboard } from "react-icons/md";
+import { FaSwatchbook } from "react-icons/fa";
+import { BiSolidSpeaker } from "react-icons/bi";
+import { FaUsers } from "react-icons/fa";
+import { Link, Route, Routes } from "react-router-dom";
+
+
+
 export default function AdminPage(){
     return(
-        <div>
-            <h1 className="bg-amber-200">Admin</h1>
+        <div className="w-full h-screen flex">
+            <div className="w-[10vw] h-full border-2 flex flex-col">
+                <Link className="">
+                    <MdDashboard/>
+                    Dashboard
+                </Link>
+                <Link to="/admin/bookings" className="">
+                    <FaSwatchbook/>
+                    Bookings
+                </Link>
+                <Link to="/admin/items" className="">
+                    <BiSolidSpeaker/>
+                    Items
+                </Link>
+                <Link className="">
+                    <FaUsers/>
+                    Users
+                </Link>
+            </div>
+        <div className="w-[calc(100vw-200px)] ">
+            <Routes path="/*">
+            <Route path="/bookings" element={<h1>Booking</h1>}/>
+            <Route path="/items" element={<h1>item</h1>}/> 
+            <Route path="/items/add" element={<h1>add item</h1>}/>
+            <Route path="/items/edit" element={<h1>Booking</h1>}/>
+            </Routes>
         </div>
+    </div>
     )
 }
